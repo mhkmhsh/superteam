f951d7f5ab0afba4901622befbbcdf249905a192
mhkmhsh <mahekmahesh12@gmail.com>
2024-05-03 11:55:43 -0400

    save

--- script.js
+++ script.js
@@ -8,21 +8,26 @@ const teamMembers = [
         biography: ' blah blah',
     },
     {
-        name: 'Lionel Messi',
-        position: 'Forward',
-        skills: ['Drippling', ' Shooting', ' Ball Control'],
-        strengths: 'Ball Control',
-        weakness: ' Heading Ability',
-        biography: ' blah blah',
+        name: 'LM JR',
+        position: 'midfielder',
+        skills: ['falling', ' Tripping', ' Crying'],
+        strengths: 'Losing',
+        weakness: ' Scoring',
+        biography: ' YEAH',
     },
 
 ]
+//team members
 
 function generateTeamCards() {
     const teamCardsContainer = document.getElementById('teamCards')
     teamMembers.forEach(member => {
         const card = document.createElement('div')
         card.classList.add('col-md-4')
+        //styling card based on position
+let backgroundColor
+
+
 const skillsList = member.skills.map(skill => `<li> ${skill} </li>`).join('')
 
         card.innerHTML = `
@@ -31,6 +36,9 @@ const skillsList = member.skills.map(skill => `<li> ${skill} </li>`).join('')
         <div class = "card-body"></div>
     <p><strong>Positions:</strong> ${member.position}</p>
     <p><strong>Skills:</strong> <ul> ${skillsList} </ul></p>
+    <p><strong>Strengths:</strong> ${member.strengths}</p>
+    <p><strong>Weakness:</strong> ${member.weakness}</p>
+    <p><strong>Biography:</strong> ${member.biography}</p>
     </div>
         `
         teamCardsContainer.appendChild(card)
