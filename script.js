ee476f209d03a5b3d61e18bda86238e89871f7e9
mhkmhsh <mahekmahesh12@gmail.com>
2024-05-03 11:20:20 -0400

    Start

---
+++ script.js
@@ -0,0 +1,24 @@
+const teamMembers = [
+    {
+        name: 'Lionel Messi',
+        position: 'Forward',
+        skills: ['Drippling', 'Shooting', 'Ball Control'],
+        strengths: 'Ball Control',
+        weakness: ' Heading Ability',
+        biography: ' blah blah',
+    },
+    {
+        name: 'Lionel Messi',
+        position: 'Forward',
+        skills: ['Drippling', 'Shooting', 'Ball Control'],
+        strengths: 'Ball Control',
+        weakness: ' Heading Ability',
+        biography: ' blah blah',
+    },
+
+]
+
+function generateTeamCards() {
+    const teamCardsContainer = document.getElementById('teamCards')
+    teamMembers.ForEach
+}
